b4fe8481fcbf8fdc34c319ea0ba70e5cadd16a63
gabriel barros <gabrielbalestrieri@hotmail.com>
2024-05-23 19:06:02 -0300

    inicial commit

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+import { Course } from '../model/course';
+import { CoursesService } from './../services/courses.service';
+import { Observable, catchError } from 'rxjs';
+import { error } from 'node:console';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.scss'
+})
+export class CoursesComponent {
+
+  courses$: Observable<Course[]>;
+  displayedColumns = ['name','category','actions'];
+
+  constructor(
+    private CoursesService: CoursesService,
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
+  ){
+  //this.courses = [];
+
+  this. courses$ = this.CoursesService.list()
+  .pipe(
+    catchError(error => {
+      this.onError('Erro ao carregar cursos')
+      return ([])
+    })
+  );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
+
+  onAdd(){
+    this.router.navigate(['new'], {relativeTo: this.route});
+  }
+}
